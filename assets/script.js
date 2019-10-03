df369640afc1b22f966119128c0f396400ae525c
Ever's PC <gomezsantiagoeverardo@gmail.com>
2019-10-03 13:29:30 -0700

    different ideas for the function that generates random word. returning that variable to html as emptyunderlines in progress

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,38 @@
 var fullWords = ["Kratos", "Atreus", "Mimir", "Baldur", "Freya", "Brok", "Faye"];
 var wins = 0;
-var targetDiv = document.getElementById("answer");
+var targetDiv =
+    document.getElementById("answer");
 var gameOver = true;
-var blankWord = []
-function gameStart() {
-    if (gameOver = true) {
-        var randNum = Math.floor(Math.random() * fullWord.length);
-        Word = fullWord[randNum];
-        gameOver = false;
-        document.getElementById("startUpTxt").textContent = "";
-        return Word;
+var gameWord = [];
+function getWord() {
+    let randNum =
+        Math.floor(Math.random() * fullWord.length);
+    Word =
+        fullWord[randNum];
+    gameOver = false;
+    document.getElementById("startUpTxt").innerHTML = "display:none";
+    gameWord = Word.toLowerCase().split("");
+    for (let i = 0; i < Word.length; i++) {
+        gameWord.push("-");
     }
-    else {
-        for (let i = 0; i < Word.length; i++) {
-            blankWord.push("_");
-            targetDiv.innerHTML(blankWord);
-            function() {
-                var x = document.getElementById("#inPut");
-                //function that uses.value to turn the value of any input 
-                //into part of a variable. will be used to verify next conditional.
-                x.value
-            }
-            if (Word.includes(myInput)) {
-                //pushes the content of the input to the targetDiv
-            }
-            else { //-1 attempts
+}
+function guesS() {
+    var letTer = document.getElementById("#inPut").value;
+    if (Word.includes(letTer)) {
+        //for{let i=0; i<word.length;i++}
+    }
+    else { //-1 attempts
+    }
 
-            }
-            if (Word.length === answer.length) {
-                gameOver = true;
-            }
-            else {
-                gameOver = false;
-            };
-            //onkeyup to start the game here??
-        }
-    }
+
+}
+if (Word.length === answer.length) {
+    gameOver = true;
+}
+else {
+    gameOver = false;
+};
+    //onkeyup to start the game here??
 };
 
 
