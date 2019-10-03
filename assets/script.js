73575492e95a6a339d7cde4384ca4dc93c72068e
Ever's PC <gomezsantiagoeverardo@gmail.com>
2019-10-03 12:01:36 -0700

    createed function that returns random word out of the array. stuck on creating separate onkeyup functions, one for starting the game and another for listening to our guesses

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,45 @@
 var fullWords = ["Kratos", "Atreus", "Mimir", "Baldur", "Freya", "Brok", "Faye"];
 var wins = 0;
 var targetDiv = document.getElementById("answer");
+var gameOver = true;
+var blankWord = []
+function gameStart() {
+    if (gameOver = true) {
+        var randNum = Math.floor(Math.random() * fullWord.length);
+        Word = fullWord[randNum];
+        gameOver = false;
+        document.getElementById("startUpTxt").textContent = "";
+        return Word;
+    }
+    else {
+        for (let i = 0; i < Word.length; i++) {
+            blankWord.push("_");
+            targetDiv.innerHTML(blankWord);
+            function() {
+                var x = document.getElementById("#inPut");
+                //function that uses.value to turn the value of any input 
+                //into part of a variable. will be used to verify next conditional.
+                x.value
+            }
+            if (Word.includes(myInput)) {
+                //pushes the content of the input to the targetDiv
+            }
+            else { //-1 attempts
 
-function getRandom(arr) {
-    var randNum = Math.floor(Math.random() * arr.length);
-    return arr[index];
+            }
+            if (Word.length === answer.length) {
+                gameOver = true;
+            }
+            else {
+                gameOver = false;
+            };
+            //onkeyup to start the game here??
+        }
+    }
 };
-var Word = getRandom(fullWords);
 
-if (document.onkeyup("")) {
-    document.getElementById(targetDiv).textContent(Word);
 
-}
+
 
 
 
