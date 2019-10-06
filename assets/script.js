41502b579439b5756ff6c25642245f464242c020
Ever's PC <gomezsantiagoeverardo@gmail.com>
2019-10-05 22:39:38 -0700

    scrapped the hangman attempt due to not enough success and time. reused major components of that code for the psychicgame assignment

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,70 @@
-var fullWords = ["Kratos", "Atreus", "Mimir", "Baldur", "Freya", "Brok", "Faye"];
+
+var fullWords = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
-var targetDiv =
-    document.getElementById("answer");
+var losses = 0;
 var gameOver = true;
-var gameWord = [];
+var guessesLeft = 10;
+var letTer;
+var myGuesses = [];
+var spanWs2 = document.getElementById("ws2");
+var spanLoss = document.getElementById("loss");
+var spanGuess = document.getElementById("gleft");
+var spanList = document.getElementById("myguess");
+gameStart();
+document.addEventListener("keyup", matchLetter);
+creatingElement(wins, spanWs2);
+creatingElement(guessesLeft, spanGuess)
+creatingElement(losses, spanLoss);
+creatingElement(myGuesses, spanList)
+// Function to build DOM Elements
+function creatingElement(variable, content) {
+    content.textContent = variable;
+}
 function getWord() {
     let randNum =
-        Math.floor(Math.random() * fullWord.length);
-    Word =
-        fullWord[randNum];
+        Math.floor(Math.random() * fullWords.length);
+    word =
+        fullWords[randNum].toLowerCase();
     gameOver = false;
-    document.getElementById("startUpTxt").innerHTML = "display:none";
-    gameWord = Word.toLowerCase().split("");
-    for (let i = 0; i < Word.length; i++) {
-        gameWord.push("-");
+    console.log(word)
+}
+function updateGuess() {
+    creatingElement(guessesLeft, spanGuess)
+    guessesLeft--;
+    myGuesses.push(letTer);
+    creatingElement(myGuesses, spanList);
+    if (gameOver == false && guessesLeft === 0) {
+        gameStart();
+        losses++;
+    }
+    else {
+
+    }
+
+}
+function matchLetter() {
+    letTer = event.key;
+
+    if (word.includes(letTer)) {
+        updateGuess();
+        wins++;
+        gameOver = true;
+        gameStart()
+        creatingElement(wins, spanWs2);
+        alert("Right answer brother!")
+    }
+    else {
+        alert("Wrong answer Chum!");
+        updateGuess();
+
+        creatingElement(losses, spanLoss);
+    };
+}
+function gameStart() {
+    if (gameOver = true) {
+        getWord();
+        gameOver = false
+        guessesLeft = 10;
     }
 }
-function guesS() {
-    var letTer = document.getElementById("#inPut").value;
-    if (Word.includes(letTer)) {
-        //for{let i=0; i<word.length;i++}
-    }
-    else { //-1 attempts
-    }
-
-
-}
-if (Word.length === answer.length) {
-    gameOver = true;
-}
-else {
-    gameOver = false;
-};
-    //onkeyup to start the game here??
-};
-
-
-
-
-
-
-
 
